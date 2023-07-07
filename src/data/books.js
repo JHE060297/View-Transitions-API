246732513224ebe1436a7691598772d9df00013e
JHE060297 <jhoan.aristizabal@uniminuto.edu.co>
2023-07-07 17:20:59 -0500

    add books

--- src/data/books.js
+++ src/data/books.js
@@ -1,7 +1,7 @@
 export const books = [
     {
         id: 'clean-code',
-        title: 'Clean code: Manual de desarrollo agil de software',
+        title: 'Clean code: A Handbook of Agile Software Craftsmanship',
         img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
         opinion: 'Un clásico entre los clásicos del mundo de la programación. Aunque en pleno 2023 yo tengo una opinión un poco polémica. No me parece un libro que recomendaría a todo el mundo, especialmente a aquellas personas que están aprendiendo. Además, tiene un tono muy dogmático y existe gente que se lo toma, peligrosamente, como la biblia del desarrollo. Pero si eres capaz de leerlo tomando perspectiva, hay cosas interesantes que todavía están vigentes. Ojo que todos los ejemplos de código están basados en Java y mucha programación orientada a objetos.',
         author: 'Robert C. Martin'
@@ -21,10 +21,25 @@ export const books = [
         author: 'Douglas Crockford'
     },
     {
-        id: 'eloquent-javaScript',
+        id: 'eloquent-javascript',
         title: 'Eloquent JavaScript: A Modern Introduction to Programming',
         img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
         opinion: '"Eloquent JavaScript" es un libro extraordinario que lleva a los lectores en un viaje apasionante a través del mundo de la programación en JavaScript. Con un enfoque amigable y accesible, el autor nos introduce en los conceptos fundamentales del lenguaje y nos guía hábilmente a través de ejercicios prácticos y desafiantes. Con una combinación equilibrada de teoría y práctica, el libro fomenta el pensamiento lógico y creativo, y nos enseña a desarrollar soluciones elegantes y eficientes. En resumen, "Eloquent JavaScript" es una guía imprescindible para aquellos que desean dominar este poderoso lenguaje de programación y explorar todo su potencial.',
         author: 'Marijn Haverbeke'
+    },
+     {
+        id: 'the-pragmatic-programmer',
+        title: 'The Pragmatic Programmer: From Journeyman to Master',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: '"The Pragmatic Programmer: From Journeyman to Master" es un libro excepcional que ha dejado una huella duradera en la comunidad de desarrollo de software. El libro ofrece una perspectiva práctica y valiosa sobre la artesanía del desarrollo de software. Lo que hace que este libro sea tan notable es su enfoque en los principios atemporales y las prácticas efectivas que trascienden las modas y las tecnologías específicas. Los autores presentan una amplia gama de temas relevantes, desde la gestión del tiempo y la depuración efectiva hasta la comunicación y la creación de un código limpio y de alta calidad.',
+        author: 'Andy Hunt y Dave Thomas'
+    },
+     {
+        id: 'desing-patterns',
+        title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348027904i/85009.jpg',
+        opinion: '"Design Patterns: Elements of Reusable Object-Oriented Software" es un libro icónico y de referencia para todo programador interesado en el diseño de software orientado a objetos. El libro presenta una colección de patrones de diseño que han demostrado ser soluciones efectivas a problemas comunes en el desarrollo de software.Una de las fortalezas de este libro es la forma en que los autores presentan cada patrón de diseño. Utilizan un enfoque claro y estructurado para explicar el propósito, la estructura, las colaboraciones y las ventajas y desventajas de cada patrón. Además, proporcionan ejemplos prácticos y aplicaciones reales para ayudar a los lectores a comprender cómo se implementan los patrones en situaciones reales.',
+        author: 'Andy Hunt y Dave Thomas'
     }
+
 ]
